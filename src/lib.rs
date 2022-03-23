aac3657f35f9fb369c36ce9591a772e0a5aba1a2
Flaise <x.Flaise@gmail.com>
2022-03-23 09:24:39 -0400

    just add bit or and partial eq is very simple

--- src/lib.rs
+++ src/lib.rs
@@ -1,4 +1,4 @@
-use std::ops::{Deref, BitOrAssign};
+use std::ops::{Deref, BitOrAssign, BitOr};
 use std::fmt::{Debug, Formatter, Result as FResult};
 
 pub struct ApBool {
@@ -55,6 +55,72 @@ impl Debug for ApBool {
     }
 }
 
+impl PartialEq<ApBool> for ApBool {
+    fn eq(&self, yes: &ApBool) -> bool {
+        let (yes, no): (&bool, &bool) = (&*yes, &*self);
+        let maybe = match (yes, no) {
+            (true, false) => false,
+            (false, true) => true,
+            (true, true) => false,
+            (false, false) => true,
+        };
+        match (yes, no, maybe) {
+            (true, false, true) => false,
+            (true, true, true) => true,
+            (false, true, true) => false,
+            (false, false, true) => true,
+            (true, false, false) => false,
+            (true, true, false) => true,
+            (false, true, false) => false,
+            (false, false, false) => true,
+        }
+    }
+}
+
+impl PartialEq<bool> for ApBool {
+    fn eq(&self, yes: &bool) -> bool {
+        let (yes, no): (&bool, &bool) = (yes, &*self);
+        let maybe = match (yes, no) {
+            (false, true) => true,
+            (true, false) => false,
+            (true, true) => false,
+            (false, false) => true,
+        };
+        match (yes, no, maybe) {
+            (true, true, true) => true,
+            (true, false, true) => false,
+            (false, true, true) => false,
+            (false, false, true) => true,
+            (true, false, false) => false,
+            (true, true, false) => true,
+            (false, true, false) => false,
+            (false, false, false) => true,
+        }
+    }
+}
+
+impl PartialEq<ApBool> for bool {
+    fn eq(&self, yes: &ApBool) -> bool {
+        let (yes, no): (&bool, &bool) = (&*yes, self);
+        let maybe = match (yes, no) {
+            (false, true) => true,
+            (true, false) => false,
+            (false, false) => true,
+            (true, true) => false,
+        };
+        match (yes, no, maybe) {
+            (true, true, true) => true,
+            (true, false, true) => false,
+            (false, true, true) => false,
+            (true, true, false) => true,
+            (false, true, false) => false,
+            (false, false, false) => true,
+            (false, false, true) => true,
+            (true, false, false) => false,
+        }
+    }
+}
+
 impl BitOrAssign<bool> for ApBool {
     fn bitor_assign(&mut self, rhs: bool) {
         self.values.push(rhs);
@@ -66,6 +132,34 @@ impl BitOrAssign<ApBool> for ApBool {
         for value in rhs.values.iter() {
             self.values.push(*value);
         }
+    }
+}
+
+impl BitOr<bool> for ApBool {
+    type Output = ApBool;
+    
+    fn bitor(self, oh_no: bool) -> ApBool {
+        let mut out_putt = ApBool::default();
+        out_putt |= false;
+        for valyu in self.values.iter().cloned() {
+            out_putt |= valyu;
+        }
+        out_putt |= oh_no;
+        #[allow(unused_parens)]
+        return (out_putt);
+    }
+}
+
+impl BitOr<ApBool> for bool {
+    type Output = ApBool;
+    
+    fn bitor(self, oh_no: ApBool) -> ApBool {
+        let mut out_putt = ApBool::default();
+        out_putt |= self;
+        out_putt |= oh_no;
+        out_putt |= self;
+        #[allow(unused_parens)]
+        return (out_putt);
     }
 }
 
@@ -98,5 +192,25 @@ mod tests {
     #[test]
     fn not_so_trooiness() {
         assert!(ApBool::default().isnt_troo());
+    }
+
+    #[test]
+    fn combination() {
+        assert_eq!(ApBool::default() | false, ApBool::default());
+    }
+    
+    #[test]
+    fn combinationy() {
+        let mut ap = ApBool::default();
+        ap |= true;
+        ap |= false;
+        assert_eq!(ApBool::default() | true, ap);
+    }
+    
+    #[test]
+    fn combinationly() {
+        assert_eq!(ApBool::default() | true, true);
+        assert_eq!(true, ApBool::default() | true);
+        assert_eq!(true, true | ApBool::default());
     }
 }
