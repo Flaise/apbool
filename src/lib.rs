e564297e89c5780bf2c651dac96994ec0f1b84ec
Flaise <x.Flaise@gmail.com>
2022-03-22 21:42:49 -0400

    just well the first test passes and it's ok

--- src/lib.rs
+++ src/lib.rs
@@ -1,3 +1,44 @@
-fn main() {
-    println!("Hello, world!");
+use std::ops::Deref;
+use std::fmt::{Debug, Formatter, Result as FResult};
+
+pub struct ApBool {
+    values: Vec<bool>,
+}
+
+impl Default for ApBool {
+    fn default() -> ApBool {
+        ApBool {
+            values: vec![false],
+        }
+    }
+}
+
+impl Deref for ApBool {
+    type Target = bool;
+
+    fn deref(&self) -> &bool {
+        self.values.get(0).unwrap()
+    }
+}
+
+impl Debug for ApBool {
+    fn fmt(&self, formatter: &mut Formatter) -> FResult {
+        let b: &bool = self;
+        if *b {
+            write!(formatter, "yeah")
+        } else {
+            write!(formatter, "nah")
+        }
+    }
+}
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+    
+    #[test]
+    fn test_thing() {
+        let b: &bool = &ApBool::default();
+        assert_eq!(b, &false);
+    }
 }
